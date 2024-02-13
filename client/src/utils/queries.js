025f9b220c38a831b99562049d31d49fc9ae49f1
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-02-13 15:01:44 +1100

    Added mutations and queries js files + code to them in client/src/utils folder

---
+++ client/src/utils/queries.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+
+	query me {
+
+		me {
+
+			_id
+			username
+			email
+			savedBooks {
+
+				title
+				authors
+				description
+				image
+				link
+
+			}
+
+		}
+
+	}
+
+`;
